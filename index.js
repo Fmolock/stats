925e2d66775228601dafedf2c82b97f87632e696
Forrest Molock <fmolock@Forrests-MacBook-Air.local>
2023-11-12 19:57:15 -0500

    first commit?

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Hello World")
+
+console.log(`You have given ${getLength(numbers)} numbers.`);
+console.log(`The sum of your numbers is ${getSum(numbers)}.`);
+console.log(`The mean of your numbers is ${getMean(numbers)}.`);
+console.log(`The smallest of your numbers is ${getMin(numbers)}.`);
+console.log(`The largest of your numbers is ${getMax(numbers)}.`);
+console.log(`The range of your numbers is ${getRange(numbers)}.`);
+console.log(`The even numbers you gave are ${getEvens(numbers)}.`);
+console.log(`The odd numbers you gave are ${getOdds(numbers)}.`);
